159c7381619afcdcfa30ae4cdca1db1ed5eb2613
dragonfarm69 <vuminhlongcm123@gmail.com>
2026-05-30 19:56:47 +0700

    added code

---
+++ backend/planner_helper/src/main/java/helper/project/planner_helper/PlannerHelperApplication.java
@@ -0,0 +1,13 @@
+package helper.project.planner_helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerHelperApplication.class, args);
+	}
+
+}
---
+++ backend/planner_helper/src/test/java/helper/project/planner_helper/PlannerHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package helper.project.planner_helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
